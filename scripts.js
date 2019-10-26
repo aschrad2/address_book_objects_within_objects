4eb4687ddc7b5b4fd9309dafcf92b0880b811409
Austin Schrader <austin.jschrader@gmail.com>
2019-10-26 16:42:38 -0700

    Built the AddressBook constructor

---
+++ scripts.js
@@ -0,0 +1,14 @@
+function AddressBook() {
+  this.contacts = []
+}
+
+funtion Contact(firstName, lastName, phoneNumber) {
+  this.firstName = firstName,
+  this.lastName = lastName,
+  this.phoneNumber = phoneNumber
+}
+
+Contact.prototype.fullName = function() {
+  return this.firstName + " " + this.lastName;
+}
+
